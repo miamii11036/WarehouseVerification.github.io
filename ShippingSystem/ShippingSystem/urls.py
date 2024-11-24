a00f83632df7cdb72d318344a695780bc8867590
miamii11036 <lmiah7492@gmail.com>
2024-11-24 14:13:15 +0000

    首頁

--- ShippingSystem/ShippingSystem/urls.py
+++ ShippingSystem/ShippingSystem/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from myweb.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+]
+
+urlpatterns = [
+    path("", index),
 ]
