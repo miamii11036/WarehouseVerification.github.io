f662d41a48e0f188f763af2381e79e15b1106d26
miamii11036 <lmiah7492@gmail.com>
2024-11-24 14:17:42 +0000

    保險

--- ShippingSystem/ShippingSystem/settings.py
+++ ShippingSystem/ShippingSystem/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "ShippingSystem.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
