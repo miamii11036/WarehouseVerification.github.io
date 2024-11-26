1cb583d71185fafa9d018f6b26d925445737fe11
miamii11036 <lmiah7492@gmail.com>
2024-11-26 18:51:00 +0000

    顯示密碼JS

--- ShippingSystem/static/js/register.js
+++ ShippingSystem/static/js/register.js
@@ -1,8 +1,18 @@
 //檢查註冊時，password欄位與confirm password欄位是否一致
-function ConfirmPassword(Password, Confirm_password, Error_message) {
-    confirm = Confirm_password.value;
+function ConfirmPassword(Password, Confirm_password, Error_message, event) {
+    Word = Password.value;
+    Cword = Confirm_password.value;
     Error_message.textContent="";
-    if (Password != confirm) {
+    if (Word != Cword) {
+        event.preventDefault();
         Error_message.textContent="密碼不一致";
     }
 }   
+//*顯示密碼*//
+function displaypassword(ShowPasswordCheckBox, Password, Confirm_password) {
+    const type = ShowPasswordCheckBox.checked ? 'text' : 'password';
+    /*checked是個布林屬性，用於表示 checkbox 是否被勾選，有選則返回true，沒選則False*/ 
+    /*?:是一個條件運算子，其運用方式為   <條件 如checked> ? 如果條件為true的值 : 如果條件為false的值 */
+    Password.type = type;
+    Confirm_password.type = type;
+}
