75dcb0a2ccef3a356690f7bbd8a8d7267e0c4014
miamii11036 <lmiah7492@gmail.com>
2024-12-02 17:50:11 +0000

    模組完成

--- ShippingSystem/myweb/models.py
+++ ShippingSystem/myweb/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class UserInfo(models.Model):
@@ -10,3 +11,29 @@ class UserInfo(models.Model):
     account = models.CharField(max_length=30)
     password = models.CharField(max_length=128)
     email = models.CharField(max_length=64)
+
+class OrderList(models.Model):
+    """
+    建立Order清單資料庫
+    """
+    id = models.AutoField(primary_key=True)
+    year = models.IntegerField()
+    month = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(12)
+        ]
+    )
+    region = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+
+class OrderDetail(models.Model):
+    """
+    建立Order內容的詳細品項清單
+    """
+    order_id = models.ForeignKey(OrderList, on_delete=models.CASCADE, related_name="items")
+    product_id = models.IntegerField()
+    product_name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    package = models.CharField()
+
